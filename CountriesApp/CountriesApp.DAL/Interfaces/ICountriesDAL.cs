bf27a44924acbc5165540b70b2f31aa0d1ec0cf2
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-07-15 01:04:22 +0400

    Added DAL interface

--- CountriesApp/CountriesApp.DAL/Interfaces/ICountriesDAL.cs
+++ CountriesApp/CountriesApp.DAL/Interfaces/ICountriesDAL.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using CountriesApp.DAL.Entities;
 
 namespace CountriesApp.DAL.Interfaces
 {
     interface ICountriesDAL
     {
+        public IQueryable<Country> GetCountries();
+        public Country GetCountry(Guid id);
+        public Country GetCountry(string name);
+        public void AddCountry(Country country);
+        public void DeleteCountry(Guid id);
+        public void UpdateCountry(Country country);
     }
 }
