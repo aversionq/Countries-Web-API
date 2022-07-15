3199098ee5591809389244fb53282e1b3ed01eb0
Arseniy K <70411996+aversionq@users.noreply.github.com>
2022-07-16 00:46:52 +0400

    Dal branch (#1)
    * Added DAL interface
    
    * Fixed DAL interface
    
    * Fixed DAL interface
    
    * Finished DAL

--- CountriesApp/CountriesApp.DAL/CountriesDAL.cs
+++ CountriesApp/CountriesApp.DAL/CountriesDAL.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using CountriesApp.DAL.Entities;
 using CountriesApp.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CountriesApp.DAL
 {
     class CountriesDAL : ICountriesDAL
     {
+        private CountriesDbContext _dbContext;
+
+        public CountriesDAL(CountriesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddCountry(Country country)
+        {
+            _dbContext.Add(country);
+            _dbContext.SaveChangesAsync();
+        }
+
+        public void DeleteCountry(Country country)
+        {
+            _dbContext.Entry(country).State = EntityState.Deleted;
+            _dbContext.SaveChangesAsync();
+        }
+
+        public IEnumerable<Country> GetCountries() => _dbContext.Countries.ToList();
+
+        public Country GetCountry(Guid id) => _dbContext.Countries.Where(x => x.Id == id).FirstOrDefault();
+
+        public Country GetCountry(string name) => _dbContext.Countries.Where(x => x.Name == name).FirstOrDefault();
+
+        public void UpdateCountry(Country country)
+        {
+            _dbContext.Update(country);
+            _dbContext.SaveChangesAsync();
+        }
     }
 }
--- CountriesApp/CountriesApp.DAL/Interfaces/ICountriesDAL.cs
+++ CountriesApp/CountriesApp.DAL/Interfaces/ICountriesDAL.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using CountriesApp.DAL.Entities;
 
 namespace CountriesApp.DAL.Interfaces
 {
     interface ICountriesDAL
     {
+        public IEnumerable<Country> GetCountries();
+        public Country GetCountry(Guid id);
+        public Country GetCountry(string name);
+        public void AddCountry(Country country);
+        public void DeleteCountry(Country country);
+        public void UpdateCountry(Country country);
     }
 }
