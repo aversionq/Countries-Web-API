1a1f7e883bec77f34f2a4d2dbd235d88b23c3e15
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-07-16 01:19:35 +0400

    Added BLL interface

--- CountriesApp/CountriesApp.BLL/Interfaces/ICountriesBLL.cs
+++ CountriesApp/CountriesApp.BLL/Interfaces/ICountriesBLL.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CountriesApp.BLL.Models;
 
 namespace CountriesApp.BLL.Interfaces
 {
     interface ICountriesBLL
     {
+        public IEnumerable<CountryDTO> GetCountries();
+        public CountryDTO GetCountry(Guid id);
+        public CountryDTO GetCountry(string name);
+        public void AddCountry(CountryDTO country);
+        public void DeleteCountry(Guid id);
+        public void UpdateCountry(CountryDTO country);
     }
 }
