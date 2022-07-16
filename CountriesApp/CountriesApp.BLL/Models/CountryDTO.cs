5f46ee7b275dd4d06faf3a11939346c59b25eec6
Arseniy K <70411996+aversionq@users.noreply.github.com>
2022-07-16 16:13:48 +0400

    Bll branch (#4)
    * Added CountryDTO
    
    * Added BLL interface
    
    * Fixed BLL interface
    
    * Finished BLL
    
    * csproj file update
    
    * Made BLL classes public

--- CountriesApp/CountriesApp.BLL/Models/CountryDTO.cs
+++ CountriesApp/CountriesApp.BLL/Models/CountryDTO.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace CountriesApp.BLL.Models
 {
-    class CountryDTO
+    public class CountryDTO
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public double Area { get; set; }
+        public int Population { get; set; }
+        public double Density { get; set; }
     }
 }
