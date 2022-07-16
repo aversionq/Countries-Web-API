758eb01c837f82f947124bdf948689614b34cea5
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-07-17 03:46:08 +0400

    Finished WebAPI

--- CountriesApp/CountriesApp.API/Controllers/CountriesController.cs
+++ CountriesApp/CountriesApp.API/Controllers/CountriesController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CountriesApp.BLL;
+using CountriesApp.BLL.Interfaces;
+using CountriesApp.BLL.Models;
 
 namespace CountriesApp.API.Controllers
 {
@@ -11,10 +14,44 @@ namespace CountriesApp.API.Controllers
     [Route("[controller]")]
     public class CountriesController : ControllerBase
     {
+        private ICountriesBLL _BLL;
 
-        private CountriesController()
+        public CountriesController()
         {
+            _BLL = new CountriesBLL();
+        }
 
+        [HttpGet]
+        [Route("getCountries")]
+        public IEnumerable<CountryDTO> GetCountries() => _BLL.GetCountries();
+
+        [HttpPost]
+        [Route("addCountry")]
+        public void AddCountry([FromBody] CountryDTO country)
+        {
+            _BLL.AddCountry(country);
+        }
+
+        [HttpGet]
+        [Route("getCountryById")]
+        public CountryDTO GetCountry(Guid id) => _BLL.GetCountry(id);
+
+        [HttpGet]
+        [Route("getCountryByName")]
+        public CountryDTO GetCountry(string name) => _BLL.GetCountry(name);
+
+        [HttpPut]
+        [Route("updateCountry")]
+        public void UpdateCountry(CountryDTO country)
+        {
+            _BLL.UpdateCountry(country);
+        }
+
+        [HttpDelete]
+        [Route("deleteCountry")]
+        public void DeleteCountry(Guid id)
+        {
+            _BLL.DeleteCountry(id);
         }
     }
 }
