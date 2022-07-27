6cb4e425952537d42ef7c35436d40804925b9691
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-07-27 05:30:13 +0400

    Tried to add unit tests (not finished)

--- CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTests.cs
+++ CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTests.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using CountriesApp.BLL.Models;
+using CountriesApp.BLL.Interfaces;
+using Xunit;
+using Moq;
 
 namespace CountriesApp.BLL.Tests
 {
     public class CountriesBLLTests
     {
+        private CountriesBLLTestsInitializer _dataInitializer;
+
+        //TODO
+        [Fact]
+        public void GetCountries_Success()
+        {
+            // Arrange
+            _dataInitializer = new CountriesBLLTestsInitializer();
+            _dataInitializer.Initialize();
+
+            var mock = new Mock<ICountriesBLL>();
+            mock.Setup(bll => bll.GetCountries()).Returns(_dataInitializer.TestData);
+
+            // Act
+
+            // Assert
+        }
     }
 }
---
+++ CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTestsInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CountriesApp.BLL.Models;
+
+namespace CountriesApp.BLL.Tests
+{
+    internal class CountriesBLLTestsInitializer
+    {
+        public IEnumerable<CountryDTO> TestData { get; private set; }
+
+        public void Initialize()
+        {
+            TestData = new List<CountryDTO>
+            {
+                new CountryDTO { Id = Guid.Parse("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"),
+                                Area = 7674,
+                                Population = 945839,
+                                Capital = "Test1",
+                                Name = "TestName",
+                                Density = 945839 / 7674},
+
+                new CountryDTO { Id = Guid.Parse("{915398DF-AAD8-4ACB-A91D-7DC774C6D786}"),
+                                 Area = 123,
+                                 Capital = "Test2",
+                                 Name = "NameTest",
+                                 Population = 326745,
+                                 Density = 326745 / 123 }
+            };
+        }
+    }
+}
