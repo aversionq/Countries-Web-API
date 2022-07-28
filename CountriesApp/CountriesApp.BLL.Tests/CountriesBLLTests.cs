8fb764a6a1d7bf486868891d127a5a5c777deb99
Arseniy K <70411996+aversionq@users.noreply.github.com>
2022-07-28 18:05:42 +0400

    BLL unit tests (#13)
    * Added blank test project with necessary dependencies
    
    * Tried to add unit tests (not finished)
    
    * Added some unit tests

---
+++ CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using CountriesApp.BLL.Models;
+using CountriesApp.DAL.Interfaces;
+using Xunit;
+using Moq;
+
+namespace CountriesApp.BLL.Tests
+{
+    public class CountriesBLLTests
+    {
+        private CountriesBLLTestsInitializer _dataInitializer;
+
+        //TODO: Add more tests.
+        [Fact]
+        public void GetCountries_Success()
+        {
+            // Arrange
+            _dataInitializer = new CountriesBLLTestsInitializer();
+            _dataInitializer.Initialize();
+
+            var mock = new Mock<ICountriesDAL>();
+            mock.Setup(x => x.GetCountries()).Returns(_dataInitializer.TestData);
+            var bll = new CountriesBLL(mock.Object);
+
+            // Act
+            var result = bll.GetCountries();
+
+            // Assert
+            var getCountriesResult = Assert.IsType<List<CountryDTO>>(result);
+        }
+
+        [Fact]
+        public void GetCountry_Success()
+        {
+            // Arrange
+            _dataInitializer = new CountriesBLLTestsInitializer();
+            _dataInitializer.Initialize();
+
+            var mock = new Mock<ICountriesDAL>();
+            mock.Setup(x => x.GetCountry(new Guid("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"))).Returns(_dataInitializer.TestData[0]);
+            var bll = new CountriesBLL(mock.Object);
+
+            // Act
+            var result = bll.GetCountry(new Guid("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"));
+
+            // Assert
+            var getCountryResult = Assert.IsType<CountryDTO>(result);
+        }
+    }
+}
---
+++ CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTestsInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CountriesApp.DAL.Entities;
+
+namespace CountriesApp.BLL.Tests
+{
+    internal class CountriesBLLTestsInitializer
+    {
+        public List<Country> TestData { get; private set; }
+
+        public void Initialize()
+        {
+            TestData = new List<Country>
+            {
+                new Country { Id = Guid.Parse("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"),
+                                Area = 7674,
+                                Population = 945839,
+                                Capital = "Test1",
+                                Name = "TestName" },
+
+                new Country { Id = Guid.Parse("{915398DF-AAD8-4ACB-A91D-7DC774C6D786}"),
+                                 Area = 123,
+                                 Capital = "Test2",
+                                 Name = "NameTest",
+                                 Population = 326745 }
+            };
+        }
+    }
+}
