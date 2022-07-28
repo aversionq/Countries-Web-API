4f162cc8da7e2b1ad74cf0e5e0b9016c52b9a387
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-07-28 18:04:58 +0400

    Added some unit tests

--- CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTests.cs
+++ CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using CountriesApp.BLL.Models;
-using CountriesApp.BLL.Interfaces;
+using CountriesApp.DAL.Interfaces;
 using Xunit;
 using Moq;
 
@@ -11,7 +11,7 @@ namespace CountriesApp.BLL.Tests
     {
         private CountriesBLLTestsInitializer _dataInitializer;
 
-        //TODO
+        //TODO: Add more tests.
         [Fact]
         public void GetCountries_Success()
         {
@@ -19,12 +19,33 @@ namespace CountriesApp.BLL.Tests
             _dataInitializer = new CountriesBLLTestsInitializer();
             _dataInitializer.Initialize();
 
-            var mock = new Mock<ICountriesBLL>();
-            mock.Setup(bll => bll.GetCountries()).Returns(_dataInitializer.TestData);
+            var mock = new Mock<ICountriesDAL>();
+            mock.Setup(x => x.GetCountries()).Returns(_dataInitializer.TestData);
+            var bll = new CountriesBLL(mock.Object);
 
             // Act
+            var result = bll.GetCountries();
 
             // Assert
+            var getCountriesResult = Assert.IsType<List<CountryDTO>>(result);
+        }
+
+        [Fact]
+        public void GetCountry_Success()
+        {
+            // Arrange
+            _dataInitializer = new CountriesBLLTestsInitializer();
+            _dataInitializer.Initialize();
+
+            var mock = new Mock<ICountriesDAL>();
+            mock.Setup(x => x.GetCountry(new Guid("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"))).Returns(_dataInitializer.TestData[0]);
+            var bll = new CountriesBLL(mock.Object);
+
+            // Act
+            var result = bll.GetCountry(new Guid("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"));
+
+            // Assert
+            var getCountryResult = Assert.IsType<CountryDTO>(result);
         }
     }
 }
--- CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTestsInitializer.cs
+++ CountriesApp/CountriesApp.BLL.Tests/CountriesBLLTestsInitializer.cs
@@ -1,31 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using CountriesApp.BLL.Models;
+using CountriesApp.DAL.Entities;
 
 namespace CountriesApp.BLL.Tests
 {
     internal class CountriesBLLTestsInitializer
     {
-        public IEnumerable<CountryDTO> TestData { get; private set; }
+        public List<Country> TestData { get; private set; }
 
         public void Initialize()
         {
-            TestData = new List<CountryDTO>
+            TestData = new List<Country>
             {
-                new CountryDTO { Id = Guid.Parse("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"),
+                new Country { Id = Guid.Parse("A313E6EF-B18B-4EAF-8B89-24B2C1A86D0D"),
                                 Area = 7674,
                                 Population = 945839,
                                 Capital = "Test1",
-                                Name = "TestName",
-                                Density = 945839 / 7674},
+                                Name = "TestName" },
 
-                new CountryDTO { Id = Guid.Parse("{915398DF-AAD8-4ACB-A91D-7DC774C6D786}"),
+                new Country { Id = Guid.Parse("{915398DF-AAD8-4ACB-A91D-7DC774C6D786}"),
                                  Area = 123,
                                  Capital = "Test2",
                                  Name = "NameTest",
-                                 Population = 326745,
-                                 Density = 326745 / 123 }
+                                 Population = 326745 }
             };
         }
     }
