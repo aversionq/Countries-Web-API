375ab90171fb1b04e7852ebaf2736bcd6dba4b4e
Arseniy K <70411996+aversionq@users.noreply.github.com>
2022-08-01 04:27:39 +0400

    Added Dependency resolver (#15)

---
+++ CountriesApp/CountriesApp.Dependencies/DependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CountriesApp.DAL.Interfaces;
+using CountriesApp.DAL;
+using CountriesApp.BLL.Interfaces;
+using CountriesApp.BLL;
+
+namespace CountriesApp.Dependencies
+{
+    public class DependencyResolver
+    {
+        #region Singleton
+        private static DependencyResolver _instance;
+
+        public static DependencyResolver Instance => _instance ??= new DependencyResolver();
+
+        private DependencyResolver() { }
+        #endregion
+
+        private ICountriesDAL _countriesDAL;
+        public ICountriesDAL CountriesDAL => _countriesDAL ??= new CountriesDAL();
+
+        private ICountriesBLL _countriesBLL;
+        public ICountriesBLL CountriesBLL => _countriesBLL ??= new CountriesBLL();
+    }
+}
